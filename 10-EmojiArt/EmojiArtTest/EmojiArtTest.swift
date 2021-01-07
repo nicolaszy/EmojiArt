e2e1aa8b051d80c8f8c98baea1dc42014453f172
Nicolas Kalousek <nicolas.kalousek@students.fhnw.ch>
2021-01-07 13:05:55 +0100

    added first 2 tests

---
+++ 10-EmojiArt/EmojiArtTest/EmojiArtTest.swift
@@ -0,0 +1,46 @@
+//
+//  EmojiArtTest.swift
+//  EmojiArtTest
+//
+//  Created by Nicolas Kalousek on 07.01.21.
+//  Copyright © 2021 fhnw. All rights reserved.
+//
+
+import XCTest
+@testable import Emoji_Art
+
+
+class EmojiArtTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAddEmoji_whenTextIsEmpty_doesNothing() throws{
+        let emojiArtDocument = Emoji_Art.EmojiArtDocument.init()
+        //check if app crashes. it should just do nothing
+        emojiArtDocument.addEmoji("", at: CGPoint(x: 10, y: 10), size: 1)
+    }
+    
+    func testAddEmoji_whenTextSizeGreaterThan1_doesNothing() throws{
+        let emojiArtDocument = Emoji_Art.EmojiArtDocument.init()
+        //check if app crashes. it should just do nothing
+        emojiArtDocument.addEmoji("", at: CGPoint(x: 10, y: 10), size: 1000)
+    }
+    
+    func testAddEmoji_whenInputValid_incrementsEmojiId() throws{
+        //TODO 
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+   
+
+}
